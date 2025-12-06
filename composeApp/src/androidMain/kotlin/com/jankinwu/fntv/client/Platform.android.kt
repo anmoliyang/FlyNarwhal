10a1745befb122018e9fa092a8378980b7d06da1
Jankin_Wu <2938567281@qq.com>
2025-12-06 23:42:58 +0800

    refactor: Refactor platform detection and add executable directory detection

--- composeApp/src/androidMain/kotlin/com/jankinwu/fntv/client/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/jankinwu/fntv/client/Platform.android.kt
@@ -1,9 +1,32 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+/*
+ * Copyright (C) 2025 FNOSP and contributors.
+ *
+ * This file has been modified from its original version.
+ * The modifications are also licensed under the AGPLv3.
+ */
 package com.jankinwu.fntv.client
 
 import android.os.Build
 
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+internal actual fun currentPlatformImpl(): Platform {
+    if (Build.SUPPORTED_ABIS == null) {
+        // unit testing
+        return Platform.Android(Arch.ARMV8A)
+    }
+    return Build.SUPPORTED_ABIS.getOrNull(0)?.let { abi ->
+        when (abi.lowercase()) {
+            "armeabi-v7a" -> Platform.Android(Arch.ARMV7A)
+            "arm64-v8a" -> Platform.Android(Arch.ARMV8A)
+            "x86_64" -> Platform.Android(Arch.X86_64)
+            else -> Platform.Android(Arch.ARMV8A)
+        }
+    } ?: Platform.Android(Arch.ARMV8A)
 }
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/jvmMain/kotlin/com/jankinwu/fntv/client/Platform.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/jankinwu/fntv/client/Platform.jvm.kt
@@ -1,7 +1,41 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+/*
+ * Copyright (C) 2025 FNOSP and contributors.
+ *
+ * This file has been modified from its original version.
+ * The modifications are also licensed under the AGPLv3.
+ */
 package com.jankinwu.fntv.client
 
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
+internal actual fun currentPlatformImpl(): Platform {
+    val os = System.getProperty("os.name")?.lowercase() ?: error("Cannot determine platform, 'os.name' is null.")
+    val arch = getArch()
+    return when {
+        "mac" in os || "os x" in os || "darwin" in os -> Platform.MacOS(arch)
+        "windows" in os -> Platform.Windows(arch)
+//        "linux" in os || "redhat" in os || "debian" in os || "ubuntu" in os -> Platform.Linux(arch)
+        "linux" in os -> Platform.Linux(arch)
+        else -> throw UnsupportedOperationException("Unsupported platform: $os")
+    }
 }
 
-actual fun getPlatform(): Platform = JVMPlatform()
+private fun getArch() = System.getProperty("os.arch").lowercase().let {
+    when {
+        "x86" in it || "x64" in it || "amd64" in it -> Arch.X86_64
+        "arm" in it || "aarch" in it -> Arch.AARCH64
+        else -> Arch.X86_64
+    }
+}
+
+fun currentPlatformDesktop(): Platform.Desktop {
+    val platform = currentPlatform()
+    check(platform is Platform.Desktop)
+    return platform
+}
