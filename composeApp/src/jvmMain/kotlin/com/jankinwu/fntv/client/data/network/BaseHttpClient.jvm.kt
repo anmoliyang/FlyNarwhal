7dde2ae36dfb05866a8631e16a9a5195476b034d
Jankin_Wu <2938567281@qq.com>
2025-12-30 01:15:16 +0800

    feat(network): Add a User-Agent header for the HTTP client

--- composeApp/src/jvmMain/kotlin/com/jankinwu/fntv/client/data/network/BaseHttpClient.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/jankinwu/fntv/client/data/network/BaseHttpClient.jvm.kt
@@ -74,6 +74,7 @@ actual val fnOfficialClient = HttpClient(OkHttp) {
         if (AccountDataCache.cookieState.isNotBlank()) {
             header(HttpHeaders.Cookie, AccountDataCache.cookieState)
         }
+        header(HttpHeaders.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36")
     }
 }
 
