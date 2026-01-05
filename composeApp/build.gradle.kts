c04dcd991a8f9863dff613424368f8cd3b3de332
Jankin_Wu <2938567281@qq.com>
2026-01-06 02:10:15 +0800

    feat(app): Update the app version to 1.6.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,8 +6,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 val osName = System.getProperty("os.name").lowercase()
 val osArch = System.getProperty("os.arch").lowercase()
 
-val appVersion = "1.5.2"
-val appVersionSuffix = ""
+val appVersion = "1.6.0"
+val appVersionSuffix = "-Beta"
 
 val platformStr = when {
     osName.contains("win") -> {
